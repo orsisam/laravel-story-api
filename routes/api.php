b1050ec6abc7816463b1a1689fd34346cf7f6694
Isro Hidayatulloh <orsi.amax@gmail.com>
2025-12-25 21:08:19 +0700

    Edit api route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StoryController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
